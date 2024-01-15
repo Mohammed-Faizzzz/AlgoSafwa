c2031bd88995ae354ead37b3fda8eafddd96a14b
Mohammed-Faizzzz <110959467+Mohammed-Faizzzz@users.noreply.github.com>
2024-01-15 21:07:10 +0800

    Add Navbar to Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Banner.css';
+import NavBar from './NavBar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <NavBar></NavBar>
+    <div className="banner-container">
+      <div className="banner">
+        <div className="banner-item" tabIndex="0">
+          <h2>AlgoSafwa's Interview Crash Course:</h2>
+          <p>System Design for Interviews and Beyond</p>
+          <button className="banner-button">Start Learning</button>
+        </div>
+        <div className="banner-item" tabIndex="0">
+          <h2>AlgoSafwa's Interview Crash Course:</h2>
+          <p>Data Structures and Algorithms</p>
+          <button className="banner-button">Start Learning</button>
+        </div>
+        <div className="banner-item" tabIndex="0">
+          <h2>Top Interview Questions</h2>
+          <button className="banner-button">Get Started</button>
+        </div>
+      </div>
     </div>
+  </>
   );
 }
 
---
+++ client/src/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './NavBar.css'; // Import the CSS styles
+
+const NavigationBar = () => {
+  return (
+    <nav className="navigation-bar">
+      <ul className="nav-list">
+        <li className="nav-clickable-item">Explore</li>
+        <li className="nav-clickable-item">Problems</li>
+        <li className="nav-clickable-item">Contest</li>
+        <li className="nav-clickable-item">Discuss</li>
+        <li className="nav-clickable-item">Store</li>
+      </ul>
+      <ul className="nav-list">
+        <li className="nav-clickable-item">Register</li>
+        <li className="nav-item">OR</li>
+        <li className="nav-clickable-item">Login</li>
+        <li className="nav-clickable-item">Premium</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
